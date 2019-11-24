80f008add4264d6a51838ffde004e99034c9867d
haruhiko-zht <44010368+haruhiko-zht@users.noreply.github.com>
2019-11-24 18:48:45 +0900

    update more useful

--- FormValidation.php
+++ FormValidation.php
@@ -1,79 +1,134 @@
 <?php
 
 
-namespace Module;
-
 /**
  * Class FormValidation
- * @package Module
+ *
+ * This class is wrapper class of InputValidation.
  */
 class FormValidation
 {
-    const REGEX_FFB_ID = InputValidation::REGEX_FFB_ID;
-    const REGEX_ENG_NUM = InputValidation::REGEX_ENG_NUM;
+  private $_inputs;
 
-    private $_inputs;
+  /**
+   * FormValidation constructor.
+   * @param bool $cookie
+   * @param array $inputs
+   * @param string $encoding
+   */
+  public function __construct(bool $cookie = false, array $inputs = [], string $encoding = 'UTF-8')
+  {
+    $this->_inputs = [];
 
-    public function __construct(bool $cookie = false, array $inputs = [], string $encoding = 'UTF-8')
-    {
-        $this->_inputs = [];
-
-        self::setInputs($_GET, $encoding);
-        self::setInputs($_POST, $encoding);
-        if ($cookie) {
-            self::setInputs($_COOKIE, $encoding);
-        }
-
-        if (!empty($inputs)) {
-            self::setInputs($inputs, $encoding);
-        }
+    self::setInputs($_GET, $encoding);
+    self::setInputs($_POST, $encoding);
+    if ($cookie) {
+      self::setInputs($_COOKIE, $encoding);
     }
 
-    private function setInputs(array $inputs, string $encoding)
-    {
-        foreach ($inputs as $key => $val) {
-            $this->_inputs[$key] = new InputValidation($val, $encoding);
-        }
+    if (!empty($inputs)) {
+      self::setInputs($inputs, $encoding);
     }
+  }
 
-    public function __get($name): string
-    {
-        if (array_key_exists($name, $this->_inputs) && $this->_inputs[$name] instanceof InputValidation) {
-            return $this->_inputs[$name]->getError() ?? '';
-        } else {
-            $this->_inputs[$name] = new InputValidation();
-            return $this->_inputs[$name]->getError() ?? '';
-        }
+  /**
+   * @param array $inputs
+   * @param string $encoding
+   */
+  private function setInputs(array $inputs, string $encoding)
+  {
+    foreach ($inputs as $key => $val) {
+      $this->_inputs[$key] = new InputValidation($val, $encoding);
     }
+  }
 
-    public function getErrCnt(): int
-    {
-        $count = 0;
-        foreach ($this->_inputs as $key => $val) {
-            if ($val->isError()) {
-                $count++;
-            }
-        }
-        return $count;
+  /**
+   * Get InputValidation $error of setting $name.
+   * @param $name
+   * @return string
+   */
+  public function __get($name): string
+  {
+    if (array_key_exists($name, $this->_inputs) && $this->_inputs[$name] instanceof InputValidation) {
+      return $this->_inputs[$name]->getError() ?? '';
+    } else {
+      $this->_inputs[$name] = new InputValidation();
+      return $this->_inputs[$name]->getError() ?? '';
     }
+  }
 
-    public function isError(): bool
-    {
-        if (self::getErrCnt() !== 0) {
-            return true;
-        } else {
-            return false;
-        }
+  /**
+   * Wrapper function of InputValidation::setError().
+   * @param $name
+   * @param string $error_msg
+   * @param bool $override
+   */
+  public function setError($name, string $error_msg, bool $override = false): void
+  {
+    if (array_key_exists($name, $this->_inputs) && $this->_inputs[$name] instanceof InputValidation) {
+      $this->_inputs[$name]->setError($error_msg, $override);
+    } else {
+      $this->_inputs[$name] = new InputValidation();
+      $this->_inputs[$name]->setError($error_msg, $override);
     }
+  }
 
-    public function validate($name): InputValidation
-    {
-        if (array_key_exists($name, $this->_inputs)) {
-            return $this->_inputs[$name];
-        } else {
-            $this->_inputs[$name] = new InputValidation();
-            return $this->_inputs[$name];
-        }
+  /**
+   * Get error count.
+   * @return int
+   */
+  public function getErrCnt(): int
+  {
+    $count = 0;
+    foreach ($this->_inputs as $key => $val) {
+      if ($val->isError()) {
+        $count++;
+      }
     }
+    return $count;
+  }
 
+  /**
+   * Return boolean, is including error.
+   * @return bool
+   */
+  public function isError(): bool
+  {
+    if (self::getErrCnt() !== 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Get validated inputs.
+   * @param array $excepts
+   * @return array
+   */
+  public function getValidatedInputs(array $excepts = []): array
+  {
+    $validated_inputs = [];
+    foreach ($this->_inputs as $key => $val) {
+      if (array_search($key, $excepts, true) === false && !$val->isError()) {
+        $validated_inputs[$key] = $val->getInput();
+      }
+    }
+    return $validated_inputs;
+  }
+
+  /**
+   * Select $value you want validation
+   * @param $name
+   * @return InputValidation
+   */
+  public function validate($name): InputValidation
+  {
+    if (array_key_exists($name, $this->_inputs)) {
+      return $this->_inputs[$name];
+    } else {
+      $this->_inputs[$name] = new InputValidation();
+      return $this->_inputs[$name];
+    }
+  }
 }
--- InputValidation.php
+++ InputValidation.php
@@ -1,241 +1,295 @@
 <?php
 
 
-namespace Module;
-
 /**
  * Class InputValidation
- * @package Module
  *
- * Single Input Validate.
- * If you want multiple input validation, you can use FormValidation class.
+ * This Class can validate single input.
+ * If you want to validate multiple inputs, you can use FormValidation class.
  */
 class InputValidation
 {
-    /**
-     * regex pattern lists
-     */
-    const REGEX_FFB_ID = '@\A[0-9a-zA-Z]{4,20}\z@';
-    const REGEX_ENG_NUM = '@\A[0-9a-zA-Z]+\z@';
+  private $value;
+  private $error;
+  private $next_valid_flag;
 
-    private $value;
-    private $error;
-    private $next_valid_flag;
+  /**
+   * InputValidation constructor.
+   * @param null $value
+   * @param string $encoding
+   */
+  public function __construct($value = null, string $encoding = 'UTF-8')
+  {
+    // init
+    if (is_string($value)) {
+      $this->value = trim($value);
+    } else {
+      $this->value = $value;
+    }
+    $this->next_valid_flag = true;
 
-    public function __construct($value = null, string $encoding = 'UTF-8')
-    {
-        if (is_string($value)) {
-            $this->value = trim($value);
-        } else {
-            $this->value = $value;
+    // set internal encoding
+    mb_internal_encoding($encoding);
+
+    // validate encoding
+    $this->validEncoding($value);
+
+    // validate null byte
+    $this->validNullByte($value);
+  }
+
+  /**
+   * Validate encoding.
+   * @param $value
+   */
+  private function validEncoding($value)
+  {
+    if (!mb_check_encoding($value)) {
+      $this->error = 'Invalid encoding.';
+    }
+  }
+
+  /**
+   * Validate null byte.
+   * @param $value
+   */
+  private function validNullByte($value)
+  {
+    if (preg_match('@\0@', $value)) {
+      $this->error = 'Including invalid letter.';
+    }
+  }
+
+  /**
+   * Set $next_valid_flag.
+   * This value is necessary when you use chain method validation.
+   * @param bool $bool
+   */
+  private function setNextValid(bool $bool = false): void
+  {
+    if ($bool) {
+      $this->next_valid_flag = true;
+    } else {
+      $this->next_valid_flag = false;
+    }
+  }
+
+  /**
+   * Check $next_valid_flag.
+   * If this value is true, next chain method validation will execute.
+   * @return bool
+   */
+  private function isNextValid(): bool
+  {
+    if ($this->next_valid_flag) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Return $value.
+   * @return null
+   */
+  public function getInput()
+  {
+    return $this->value;
+  }
+
+  /**
+   * Set $value, and reset $error/$next_valid_flag.
+   * Basically, this function should not be used.
+   * @param $value
+   * @param bool $reset_error
+   * @return InputValidation
+   */
+  public function updateInput($value, bool $reset_error = false): InputValidation
+  {
+    $this->value = $value;
+    if ($reset_error) {
+      self::setNextValid(true);
+      self::setError(null, true);
+    }
+    return $this;
+  }
+
+  /**
+   * Set $error.
+   * If you want add original error, you can use this.
+   * But maybe recommend using self::handler() or FormValidation class.
+   * @param string $error_msg
+   * @param bool $override
+   */
+  public function setError(string $error_msg, bool $override = false): void
+  {
+    if ($override || !isset($this->error)) {
+      $this->error = $error_msg;
+    }
+  }
+
+  /**
+   * Return $error.
+   * @return string
+   */
+  public function getError(): string
+  {
+    return $this->error ?? '';
+  }
+
+  /**
+   * Return boolean, is $value including $error.
+   * @return bool
+   */
+  public function isError(): bool
+  {
+    if (isset($this->error)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+
+  /**
+   * Make original validation.
+   * If you want original validation, you can use this.
+   *
+   * ex)
+   * handler(function ($value, $error) {
+   *  if (!$value) {
+   *      $error(message);
+   *  }
+   * })
+   *
+   * @param callable $handler
+   * @return InputValidation
+   */
+  public function handler(callable $handler): InputValidation
+  {
+    if (self::isNextValid()) {
+      $error = function ($error_msg, bool $override = false) {
+        self::setError($error_msg, $override);
+        self::setNextValid();
+      };
+      $handler($this->value, $error);
+    }
+    return $this;
+  }
+
+  public function notEmpty(): InputValidation
+  {
+    if (self::isNextValid()) {
+      if (empty($this->value)) {
+        self::setNextValid();
+        self::setError('Required.');
+      }
+    }
+    return $this;
+  }
+
+  public function ifNotEmpty(): InputValidation
+  {
+    if (self::isNextValid()) {
+      if (empty($this->value)) {
+        self::setNextValid();
+      }
+    }
+    return $this;
+  }
+
+  public function minVal($value): InputValidation
+  {
+    if (self::isNextValid()) {
+      if ($this->value < $value) {
+        self::setNextValid();
+        self::setError("Required greater equal than {$value}.");
+      }
+    }
+    return $this;
+  }
+
+  public function maxVal($value): InputValidation
+  {
+    if (self::isNextValid()) {
+      if ($this->value > $value) {
+        self::setNextValid();
+        self::setError("Required smaller equal than {$value}.");
+      }
+    }
+    return $this;
+  }
+
+  public function betweenVal($min, $max): InputValidation
+  {
+    if (self::isNextValid()) {
+      if ($this->value < $min || $this->value > $max) {
+        self::setNextValid();
+        self::setError("Required between {$min} and {$max}.");
+      }
+    }
+    return $this;
+  }
+
+  public function minLen(int $value): InputValidation
+  {
+    if (self::isNextValid()) {
+      if (mb_strlen($this->value) < $value) {
+        self::setNextValid();
+        self::setError("Required greater equal than {$value} characters.");
+      }
+    }
+    return $this;
+  }
+
+  public function maxLen(int $value): InputValidation
+  {
+    if (self::isNextValid()) {
+      if (mb_strlen($this->value) > $value) {
+        self::setNextValid();
+        self::setError("Required smaller equal than {$value} characters.");
+      }
+    }
+    return $this;
+  }
+
+  public function betweenLen(int $min, int $max): InputValidation
+  {
+    if (self::isNextValid()) {
+      if (mb_strlen($this->value) < $min || mb_strlen($this->value) > $max) {
+        self::setNextValid();
+        self::setError("Required between {$min} and {$max} characters.");
+      }
+    }
+    return $this;
+  }
+
+  public function regex(string $pattern): InputValidation
+  {
+    if (self::isNextValid()) {
+      if (!preg_match($pattern, $this->value)) {
+        self::setNextValid();
+        self::setError("Invalid input.");
+      }
+    }
+    return $this;
+  }
+
+  public function equal($value, bool $strict = true): InputValidation
+  {
+    if (self::isNextValid()) {
+      if ($strict) {
+        if ($this->value !== $value) {
+          self::setNextValid();
+          self::setError("Dont match.");
         }
-        $this->next_valid_flag = true;
-
-        // set internal encoding
-        mb_internal_encoding($encoding);
-
-        // validate encoding
-        $this->validEncoding($value);
-
-        // validate null byte
-        $this->validNullByte($value);
-    }
-
-    private function validEncoding($value)
-    {
-        if (!mb_check_encoding($value)) {
-            $this->error = 'Invalid encoding.';
+      } else {
+        if ($this->value != $value) {
+          self::setNextValid();
+          self::setError("Dont match.");
         }
+      }
     }
-
-    private function validNullByte($value)
-    {
-        if (preg_match('@\0@', $value)) {
-            $this->error = 'Including invalid letter.';
-        }
-    }
-
-    private function setNextValid(bool $bool = false): void
-    {
-        if ($bool) {
-            $this->next_valid_flag = true;
-        } else {
-            $this->next_valid_flag = false;
-        }
-    }
-
-    private function isNextValid(): bool
-    {
-        if ($this->next_valid_flag) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public function getInput()
-    {
-        return $this->value;
-    }
-
-    public function updateInput($value, bool $reset_error = false): InputValidation
-    {
-        $this->value = $value;
-        if ($reset_error) {
-            self::setNextValid(true);
-            self::setError(null, true);
-        }
-        return $this;
-    }
-
-    public function setError(string $error_msg, bool $override = false): void
-    {
-        if ($override || !isset($this->error)) {
-            $this->error = $error_msg;
-        }
-    }
-
-    public function getError(): string
-    {
-        return $this->error ?? '';
-    }
-
-    public function isError(): bool
-    {
-        if (isset($this->error)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
-    public function handler(callable $handler): InputValidation
-    {
-        if(self::isNextValid()){
-            $error = function ($error_msg, bool $override = false) {
-                self::setError($error_msg, $override);
-                self::setNextValid();
-            };
-            $handler($this->value, $error);
-        }
-        return $this;
-    }
-
-    public function notEmpty(): InputValidation
-    {
-        if (self::isNextValid()) {
-            if (empty($this->value)) {
-                self::setNextValid();
-                self::setError('Required.');
-            }
-        }
-        return $this;
-    }
-
-    public function ifNotEmpty(): InputValidation
-    {
-        if (self::isNextValid()) {
-            if (empty($this->value)) {
-                self::setNextValid();
-            }
-        }
-        return $this;
-    }
-
-    public function minVal($value): InputValidation
-    {
-        if (self::isNextValid()) {
-            if ($this->value < $value) {
-                self::setNextValid();
-                self::setError("Required greater equal than {$value}.");
-            }
-        }
-        return $this;
-    }
-
-    public function maxVal($value): InputValidation
-    {
-        if (self::isNextValid()) {
-            if ($this->value > $value) {
-                self::setNextValid();
-                self::setError("Required smaller equal than {$value}.");
-            }
-        }
-        return $this;
-    }
-
-    public function betweenVal($min, $max): InputValidation
-    {
-        if (self::isNextValid()) {
-            if ($this->value < $min || $this->value > $max) {
-                self::setNextValid();
-                self::setError("Required between {$min} and {$max}.");
-            }
-        }
-        return $this;
-    }
-
-    public function minLen(int $value): InputValidation
-    {
-        if (self::isNextValid()) {
-            if (mb_strlen($this->value) < $value) {
-                self::setNextValid();
-                self::setError("Required greater equal than {$value} characters.");
-            }
-        }
-        return $this;
-    }
-
-    public function maxLen(int $value): InputValidation
-    {
-        if (self::isNextValid()) {
-            if (mb_strlen($this->value) > $value) {
-                self::setNextValid();
-                self::setError("Required smaller equal than {$value} characters.");
-            }
-        }
-        return $this;
-    }
-
-    public function betweenLen(int $min, int $max): InputValidation
-    {
-        if (self::isNextValid()) {
-            if (mb_strlen($this->value) < $min || mb_strlen($this->value) > $max) {
-                self::setNextValid();
-                self::setError("Required between {$min} and {$max} characters.");
-            }
-        }
-        return $this;
-    }
-
-    public function regex(string $pattern): InputValidation
-    {
-        if (self::isNextValid()) {
-            echo '^^';
-            if (!preg_match($pattern, $this->value)) {
-                self::setNextValid();
-                self::setError("Invalid input.");
-            }
-        }
-        return $this;
-    }
-
-    public function equal($value, bool $strict = true): InputValidation
-    {
-        if (self::isNextValid()) {
-            if ($strict) {
-                if ($this->value !== $value) {
-                    self::setNextValid();
-                    self::setError("Dont match.");
-                }
-            } else {
-                if ($this->value != $value) {
-                    self::setNextValid();
-                    self::setError("Dont match.");
-                }
-            }
-        }
-        return $this;
-    }
+    return $this;
+  }
 }
